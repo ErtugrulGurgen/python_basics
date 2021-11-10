ba56265f53fcdf85f7fba8a9521258a450a48fd0
ertugrul gurgen <88859001+ErtugrulGurgen@users.noreply.github.com>
2021-11-10 21:28:49 +0100

    Add files via upload

---
+++ Password_Reminder.py
@@ -0,0 +1,6 @@
+name = input("Please enter your first name: ")
+
+if name == "Ertugrul":
+    print("Hello, Ertugrul! The password is: Qwer0987")
+else:
+    print("Hello," , name + "! See you later.")
